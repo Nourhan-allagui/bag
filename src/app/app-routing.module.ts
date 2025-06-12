31521aa8e8a18849d1174588e58842da29096e07
nourhen allagui <nourhane.allagui@gmail.com>
2025-06-12 12:33:37 +0100

    improve the test code.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ShopComponent} from './shop/shop.component';
 
 const routes: Routes = [
-  //{ path: 'home', component: HomeComponent },
-  // { path: 'shop', component: ShopComponent },
+  { path: 'home', component: HomeComponent },
+   { path: 'shop', component: ShopComponent },
   // { path: 'features', component: FeaturesComponent },
   // { path: 'blog', component: BlogComponent },
   // { path: 'about', component: AboutComponent },
-  // { path: 'contact', component: ContactComponent }
+  // { path: 'contact', component: ContactComponent },
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
