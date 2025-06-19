b403d1e43d01e030f066622f801ccd835bf6f97d
nourhen allagui <nourhane.allagui@gmail.com>
2025-06-19 21:44:19 +0100

    improve the about style with the rest of the pages .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ShopComponent} from './shop/shop.component';
 import {FeatureComponent} from './feature/feature.component';
+import {AboutComponent} from './about/about.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
    { path: 'shop', component: ShopComponent },
    { path: 'features', component: FeatureComponent },
   // { path: 'blog', component: BlogComponent },
-  // { path: 'about', component: AboutComponent },
+   { path: 'about', component: AboutComponent },
   // { path: 'contact', component: ContactComponent },
   { path: '**', component: HomeComponent },
 ];
