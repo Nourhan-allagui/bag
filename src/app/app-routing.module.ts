2774edd234bcaeb4cdc3afc5242cb70a16508522
nourhen allagui <nourhane.allagui@gmail.com>
2025-06-22 23:29:48 +0100

    fix small errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,6 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
    { path: 'shop', component: ShopComponent },
    { path: 'features', component: FeatureComponent },
-  // { path: 'blog', component: BlogComponent },
    { path: 'about', component: AboutComponent },
    { path: 'contact', component: ContactComponent },
   { path: '**', component: HomeComponent },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,15 +27,11 @@ export class AppComponent {
       componentRef.addToCartDetails.subscribe((data: any) => {
         this.latestCartItem = data;
         console.log('Received from ShopComponent:', data);
-
-        // 🔥 Pass cart to FeatureComponent if that's what got activated
         if ('selectedDetails' in componentRef) {
           componentRef.selectedDetails = data;
         }
       });
     }
-
-    // Optional: cover case if FeatureComponent is routed directly (and not through ShopComponent)
     if ('selectedDetails' in componentRef && this.latestCartItem) {
       componentRef.selectedDetails = this.latestCartItem;
     }
